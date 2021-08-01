a6e2713fefc0674ffc07635668bbd1853e4d7a5b
Marina Dariy <dariy.mvs@gmail.com>
2021-08-01 20:38:20 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,95 @@
+const Koa = require('koa');
+const koaBody = require('koa-body');
+const cors = require('@koa/cors');
+// const http = require('http');
+
+const app = new Koa();
+
+app.use(koaBody({
+  urlencoded: true,
+  text: true,
+  json: true,
+  multipart: true,
+}));
+
+app.use(cors());
+
+const tickets = new Map();
+
+app.use(async (ctx) => {
+  if (ctx.request.method === 'GET') {
+    let { id } = ctx.request.query;
+    const { method } = ctx.request.query;
+    id = +id;
+    switch (method) {
+      case 'allTickets': {
+        const jsonArr = Array.from(tickets.values());
+        const resp = jsonArr.map((el) => ({
+          id: el.id,
+          name: el.name,
+          status: el.status,
+          created: el.created,
+        }));
+          // console.log(jsonObject);
+        ctx.response.body = resp;
+        return;
+      }
+      case 'ticketById': {
+        // console.log(Array.from(tickets.values()))
+        const answer = JSON.stringify(tickets.get(id));
+        // console.log(answer);
+        ctx.response.body = answer;
+        return;
+      }
+      case 'status': {
+        const { status } = ctx.request.query;
+        // console.log(status);
+        tickets.get(id).status = status;
+        ctx.body = { ok: true };
+        return;
+      }
+      case 'delete': {
+        tickets.delete(+id);
+        ctx.body = { ok: true };
+        return;
+      }
+      default: {
+        ctx.response.status = 404;
+        return;
+      }
+    }
+  } else if (ctx.request.method === 'POST') {
+    // console.log(ctx.request.body);
+    const { taskName, taskDescr, created } = ctx.request.body;
+    const ticketId = tickets.size + 1;
+    tickets.set(ticketId, {
+      id: ticketId,
+      name: taskName,
+      status: false,
+      created,
+      description: taskDescr,
+    });
+    // console.log(Array.from(tickets.values()));
+    ctx.body = { ok: true };
+    return;
+  } else if (ctx.request.method === 'PUT') {
+    const { id, taskName, taskDescr } = ctx.request.body;
+    const ticket = tickets.get(+id);
+    ticket.name = taskName;
+    ticket.description = taskDescr;
+    // console.log(tickets.get(id));
+    ctx.body = { ok: true };
+    return;
+  } else if (ctx.request.method === 'PUT') {
+    const { id, taskName, taskDescr } = ctx.request.body;
+    const ticket = tickets.get(+id);
+    ticket.name = taskName;
+    ticket.description = taskDescr;
+    // console.log(tickets.get(id));
+    ctx.body = { ok: true };
+    return;
+  }
+  ctx.body = { status: 'OK' };
+});
+
+app.listen(7070, () => console.log('Server is works'));
